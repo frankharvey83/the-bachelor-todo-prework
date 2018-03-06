5ebac0781cd2d7d7852170d666898095f716473f
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-06 01:10:31 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -9,13 +9,13 @@ winner = ""
           hash.each do |key, value|
             if key == "name"
               winner << value
-              winner.split[0]
             end
           end
         end
       end
     end
   end
+  winner.split[0]
 end
 
 def get_contestant_name(data, occupation)
