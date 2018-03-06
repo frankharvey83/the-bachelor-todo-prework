0b9ccca0b49e470d8435bc7bb8f85eb5a2c33516
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-06 01:59:50 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,19 +1,84 @@
+require 'pry'
+
 def get_first_name_of_season_winner(data, season)
-  # code here
+winner = ""
+
+  data.each do |season_num, array|
+    if season_num == season
+    array.each do |hash|
+      if hash["status"] == "Winner"
+          hash.each do |key, value|
+            if key == "name"
+              winner << value
+              end
+            end
+          end
+        end
+      end
+    end
+  winner.split[0]
 end
 
 def get_contestant_name(data, occupation)
-  # code here
+  winner = ""
+
+    data.each do |season_num, array|
+      array.each do |hash|
+        if hash["occupation"] == occupation
+            hash.each do |key, value|
+              if key == "name"
+                winner << value
+              end
+          end
+        end
+      end
+    end
+  winner
 end
 
 def count_contestants_by_hometown(data, hometown)
-  # code here
+  counter = 0
+
+  data.each do |season_num, array|
+    array.each do |hash|
+      if hash["hometown"] == hometown
+        counter += 1
+       end
+     end
+   end
+  counter
 end
 
 def get_occupation(data, hometown)
-  # code here
+    data.each do |season_num, array|
+      array.each do |hash|
+        if hash["hometown"] == hometown
+          hash.each do |key, value|
+            if key == "occupation"
+              return value
+          end
+        end
+      end
+    end
+  end
 end
 
 def get_average_age_for_season(data, season)
-  # code here
+  ages = 0
+  counter = 0
+
+  data.each do |season_num, array|
+    if season_num == season
+      array.each do |hash|
+        hash.each do |key, value|
+          if key == "age"
+            counter += 1
+              ages += value.to_i
+          end
+        end
+      end
+    end
+  end
+average = ages.to_f/ counter
+average.round
 end
